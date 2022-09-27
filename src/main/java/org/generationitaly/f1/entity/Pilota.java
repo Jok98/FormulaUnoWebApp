98048e90c0198db3119e71103a711f51703bec5b
KalashniRob <robert.c.matei@outlook.com>
2022-09-27 10:26:05 +0200

    Relazioni con Monoposto, Scuderia, Gara, ClassificaPiloti

--- src/main/java/org/generationitaly/f1/entity/Pilota.java
+++ src/main/java/org/generationitaly/f1/entity/Pilota.java
@@ -2,48 +2,69 @@ package org.generationitaly.f1.entity;
 
 import java.sql.Date;
 
+import org.generationitaly.f1.entity.Gara;
+import org.generationitaly.f1.entity.ClassificaPiloti;
+import org.generationitaly.f1.entity.Scuderia;
+import org.generationitaly.f1.entity.Monoposto;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "pilota")
 public class Pilota {
-	
+
 	@Column(name = "foto", length = 6000, nullable = false)
 	private String foto;
-	
+
 	@Column(name = "nome", length = 45, nullable = false)
 	private String nome;
-	
+
 	@Column(name = "cognome", length = 45, nullable = false)
 	private String cognome;
-	
+
 	@Column(name = "dataNascita", nullable = false)
+	@Temporal(TemporalType.DATE)
 	private Date dataNascita;
-	
+
 	@Column(name = "luogoNascita", length = 45, nullable = false)
 	private String luogoNascita;
-	
+
 	@Column(name = "nazionalita", length = 45, nullable = false)
 	private String nazionalita;
-	
+
 	@Id
 	@Column(name = "numeroPilota", nullable = false)
 	private int numeroPilota;
-	
-	@Column(name = "titoliMondiali", length = 45, nullable = false)
+
+	@Column(name = "titoliMondiali", nullable = false)
 	private int titoliMondiali;
-	
+
 	@Column(name = "profiloSocial", length = 6000, nullable = false)
 	private String profiloSocial;
-	
+
 	@Column(name = "biografia", length = 6000, nullable = false)
 	private String biografia;
-	
-	@Column(name = "nomeScuderia", length = 45, nullable = false)
-	private String nomeScuderia;
+
+	@ManyToOne
+	@JoinColumn(name = "nomeScuderia", nullable = false)
+	private Scuderia scuderia;
+
+	@OneToMany(mappedBy = "pilota")
+	private Gara gara;
+
+	@OneToOne(mappedBy = "pilota")
+	private Monoposto monoposto;
+
+	@OneToOne(mappedBy = "pilota")
+	private ClassificaPiloti classificaPiloti;
 
 	public String getFoto() {
 		return foto;
@@ -125,22 +146,44 @@ public class Pilota {
 		this.biografia = biografia;
 	}
 
-	public String getNomeScuderia() {
-		return nomeScuderia;
+	public Scuderia getScuderia() {
+		return scuderia;
 	}
 
-	public void setNomeScuderia(String nomeScuderia) {
-		this.nomeScuderia = nomeScuderia;
+	public void setScuderia(Scuderia scuderia) {
+		this.scuderia = scuderia;
+	}
+
+	public Gara getGara() {
+		return gara;
+	}
+
+	public void setGara(Gara gara) {
+		this.gara = gara;
+	}
+
+	public Monoposto getMonoposto() {
+		return monoposto;
+	}
+
+	public void setMonoposto(Monoposto monoposto) {
+		this.monoposto = monoposto;
+	}
+
+	public ClassificaPiloti getClassificaPiloti() {
+		return classificaPiloti;
+	}
+
+	public void setClassificaPiloti(ClassificaPiloti classificaPiloti) {
+		this.classificaPiloti = classificaPiloti;
 	}
 
 	@Override
 	public String toString() {
-		return "Pilota [foto=" + foto + ", nome=" + nome + ", cognome=" + cognome + ", dataNascita=" + dataNascita
-				+ ", luogoNascita=" + luogoNascita + ", nazionalita=" + nazionalita + ", numeroPilota=" + numeroPilota
+		return "Pilota [numeroPilota=" + numeroPilota + ", foto=" + foto + ", nome=" + nome + ", cognome=" + cognome
+				+ ", dataNascita=" + dataNascita + ", luogoNascita=" + luogoNascita + ", nazionalita=" + nazionalita
 				+ ", titoliMondiali=" + titoliMondiali + ", profiloSocial=" + profiloSocial + ", biografia=" + biografia
-				+ ", nomeScuderia=" + nomeScuderia + "]";
+				+ "]";
 	}
-	
-	
-	
+
 }
