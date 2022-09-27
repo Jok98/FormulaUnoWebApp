e98773639c94fec8990ee0d1c8d3a06c91bb5406
KalashniRob <robert.c.matei@outlook.com>
2022-09-27 10:16:18 +0200

    Update Monoposto

--- src/main/java/org/generationitaly/f1/entity/Monoposto.java
+++ src/main/java/org/generationitaly/f1/entity/Monoposto.java
@@ -2,6 +2,8 @@ package org.generationitaly.f1.entity;
 
 import java.sql.Date;
 
+import org.generationitaly.f1.entity.Pilota;
+import org.generationitaly.f1.entity.Scuderia;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,88 +11,91 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "monoposto")
 public class Monoposto {
 
-		@Id
-		@Column(name = "idVettura", length = 45, nullable = false)
-		private String idVettura;
-		
-		@Column(name = "motore", length = 45, nullable = false)
-		private String motore;
-		
-		@Column(name = "numeroVittorie", nullable = false)
-		private int numeroVittorie;
-		
-		@Column(name = "annoProduzione", nullable = false)
-		private Date annoProduzione;
-		
-		@OneToOne
-		@JoinColumn(name = "numeroPilota", nullable = false)
-		private Pilota pilota;
-		
-		@ManyToOne
-		@JoinColumn(name = "Scuderia_nomeScuderia", nullable = false)
-		private Scuderia scuderia;
-		
-		@Column(name = "fotoMonoposto", nullable = false)
-		private String fotoMonoposto;
+	@Id
+	@Column(name = "idVettura", length = 45, nullable = false)
+	private String idVettura;
 
-		public String getIdVettura() {
-			return idVettura;
-		}
+	@Column(name = "motore", length = 45, nullable = false)
+	private String motore;
 
-		public void setIdVettura(String idVettura) {
-			this.idVettura = idVettura;
-		}
+	@Column(name = "numeroVittorie", nullable = false)
+	private int numeroVittorie;
 
-		public String getMotore() {
-			return motore;
-		}
+	@Column(name = "annoProduzione", nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date annoProduzione;
 
-		public void setMotore(String motore) {
-			this.motore = motore;
-		}
+	@OneToOne
+	@JoinColumn(name = "numeroPilota", nullable = false)
+	private Pilota pilota;
 
-		public int getNumeroVittorie() {
-			return numeroVittorie;
-		}
+	@ManyToOne
+	@JoinColumn(name = "nomeScuderia", nullable = false)
+	private Scuderia scuderia;
 
-		public void setNumeroVittorie(int numeroVittorie) {
-			this.numeroVittorie = numeroVittorie;
-		}
+	@Column(name = "fotoMonoposto", length = 6000, nullable = false)
+	private String fotoMonoposto;
 
-		public Date getAnnoProduzione() {
-			return annoProduzione;
-		}
+	public String getIdVettura() {
+		return idVettura;
+	}
 
-		public void setAnnoProduzione(Date annoProduzione) {
-			this.annoProduzione = annoProduzione;
-		}
+	public void setIdVettura(String idVettura) {
+		this.idVettura = idVettura;
+	}
 
-		public Pilota getPilota() {
-			return pilota;
-		}
+	public String getMotore() {
+		return motore;
+	}
 
-		public void setPilota(Pilota pilota) {
-			this.pilota = pilota;
-		}
+	public void setMotore(String motore) {
+		this.motore = motore;
+	}
 
-		public Scuderia getScuderia() {
-			return scuderia;
-		}
+	public int getNumeroVittorie() {
+		return numeroVittorie;
+	}
 
-		public void setScuderia(Scuderia scuderia) {
-			this.scuderia = scuderia;
-		}
+	public void setNumeroVittorie(int numeroVittorie) {
+		this.numeroVittorie = numeroVittorie;
+	}
 
-		public String getFotoMonoposto() {
-			return fotoMonoposto;
-		}
+	public Date getAnnoProduzione() {
+		return annoProduzione;
+	}
 
-		public void setFotoMonoposto(String fotoMonoposto) {
-			this.fotoMonoposto = fotoMonoposto;
-		}		
+	public void setAnnoProduzione(Date annoProduzione) {
+		this.annoProduzione = annoProduzione;
+	}
+
+	public Pilota getPilota() {
+		return pilota;
+	}
+
+	public void setPilota(Pilota pilota) {
+		this.pilota = pilota;
+	}
+
+	public Scuderia getScuderia() {
+		return scuderia;
+	}
+
+	public void setScuderia(Scuderia scuderia) {
+		this.scuderia = scuderia;
+	}
+
+	public String getFotoMonoposto() {
+		return fotoMonoposto;
+	}
+
+	public void setFotoMonoposto(String fotoMonoposto) {
+		this.fotoMonoposto = fotoMonoposto;
+	}
 }
