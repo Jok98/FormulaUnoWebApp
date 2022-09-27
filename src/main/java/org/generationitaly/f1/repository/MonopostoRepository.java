b38a4092a7a09a70105d93302de3a416580de521
KalashniRob <robert.c.matei@outlook.com>
2022-09-27 11:34:14 +0200

    Creazione di Repository e Impl

---
+++ src/main/java/org/generationitaly/f1/repository/MonopostoRepository.java
@@ -0,0 +1,7 @@
+package org.generationitaly.f1.repository;
+
+import org.generationitaly.f1.entity.Monoposto;
+
+public interface MonopostoRepository extends CrudRepository<Monoposto, String> {
+
+}
---
+++ src/main/java/org/generationitaly/f1/repository/ScuderiaRepository.java
@@ -0,0 +1,7 @@
+package org.generationitaly.f1.repository;
+
+import org.generationitaly.f1.entity.Scuderia;
+
+public interface ScuderiaRepository extends CrudRepository<Scuderia, String> {
+
+}
