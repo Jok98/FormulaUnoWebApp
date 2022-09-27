d14f497c230703ab93ef5f95fee056cfcf29d03c
KalashniRob <robert.c.matei@outlook.com>
2022-09-27 10:08:58 +0200

    Relazioni con Pilota, Circuito

--- src/main/java/org/generationitaly/f1/entity/Gara.java
+++ src/main/java/org/generationitaly/f1/entity/Gara.java
@@ -1,22 +1,24 @@
 package org.generationitaly.f1.entity;
 
 import java.sql.Date;
-//import java.util.List;
+import java.util.List;
 
+import org.generationitaly.f1.entity.Pilota;
+import org.generationitaly.f1.entity.Circuito;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="Gara")
+@Table(name = "gara")
 public class Gara {
-	
+
 //	CREATE TABLE IF NOT EXISTS `F1`.`Gara` (
 //	  `idGara` INT NOT NULL,
 //	  `listaPiloti` VARCHAR(45) NOT NULL,
@@ -37,75 +39,86 @@ public class Gara {
 //	    FOREIGN KEY (`pilotaNumero`)
 //	    REFERENCES `F1`.`Pilota` (`numeroPilota`)
 
-	
 	@Id
-	@Column(name="idGara", nullable=false)
+	@Column(name = "idGara", nullable = false)
 	private int idGara;
-	
-	@Column(name="date", nullable=false)
+
+	@Column(name = "date", nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date date;
-	
-	
-	@Column(name="numeroGiri", nullable=false)
-	private int numeroGiri;
-	
-	
-	@OneToOne
-	@JoinColumn(name="nomeCircuito", nullable=false)
-	private Circuito circuito;
-	
-	
-	@Column(name="lsitaPiloti", nullable=false)
-	private List<Pilota> listaPiloti;
-	
-	@Column(name="grigliaDiPartenza", nullable=false)
-	private List<Pilota> grigliaDiPartenza;
-	
-	@ManyToOne
-	@JoinColumn(name="pilotaNumero", nullable=false)
-	private Pilota pilota;
 
+	@Column(name = "numeroGiri", nullable = false)
+	private int numeroGiri;
+
+	@OneToOne
+	@JoinColumn(name = "nomeCircuito", nullable = false)
+	private Circuito circuito;
+
+	@Column(name = "listaPiloti", nullable = false)
+	private List<Pilota> listaPiloti;
+
+	@Column(name = "grigliaDiPartenza", nullable = false)
+	private List<Pilota> grigliaDiPartenza;
+
+	@ManyToOne
+	@JoinColumn(name = "pilotaNumero", nullable = false)
+	private Pilota pilota;
 
 	public int getIdGara() {
 		return idGara;
 	}
 
-
 	public void setIdGara(int idGara) {
 		this.idGara = idGara;
 	}
 
+	public List<Pilota> getListaPiloti() {
+		return listaPiloti;
+	}
+
+	public void setListaPiloti(List<Pilota> listaPiloti) {
+		this.listaPiloti = listaPiloti;
+	}
+
+	public List<Pilota> getGrigliaDiPartenza() {
+		return grigliaDiPartenza;
+	}
+
+	public void setGrigliaDiPartenza(List<Pilota> grigliaDiPartenza) {
+		this.grigliaDiPartenza = grigliaDiPartenza;
+	}
+
+	public Pilota getPilota() {
+		return pilota;
+	}
+
+	public void setPilota(Pilota pilota) {
+		this.pilota = pilota;
+	}
 
 	public Date getDate() {
 		return date;
 	}
 
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
 
 	public int getNumeroGiri() {
 		return numeroGiri;
 	}
 
-
 	public void setNumeroGiri(int numeroGiri) {
 		this.numeroGiri = numeroGiri;
 	}
-
 
 	public Circuito getCircuito() {
 		return circuito;
 	}
 
-
 	public void setCircuito(Circuito circuito) {
 		this.circuito = circuito;
 	}
-
 
 	@Override
 	public String toString() {
@@ -113,4 +126,3 @@ public class Gara {
 				+ "]";
 	}
 }
-	
